801493ef14ac7496c6c9a73088c900974f39b651
aveselinovic <aveselinovic.90@gmail.com>
2015-07-14 15:54:26 +0200

    adding the Variables class example

---
+++ src/main/java/com/java/javasetutorial/Variables.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.javasetutorial;
+
+/**
+ *
+ * @author aveselinovic
+ */
+public class Variables {
+     
+    public static void main(String[] args){
+      int myNumber = 88;
+      short myShort = 847;
+      long myLong = 979723;
+      
+      double myDouble = 5.322;
+      float myFloat = 324.3f;
+      
+      char myChar = 'y';
+      boolean myBool = false;
+      
+      byte myByte= 127;
+      
+      System.out.println(myNumber);
+      System.out.println(myShort);
+      System.out.println(myLong);
+      System.out.println(myDouble);
+      System.out.println(myFloat);
+      System.out.println(myChar);
+      System.out.println(myBool);
+      System.out.println(myByte);
+    }
+  
+    
+}
